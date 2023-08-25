e2c0f5979ca1501ef3653bd398e7d23815074afe
digitalblender <david@digitalblender.co>
2023-08-24 21:44:12 -0700

    adds storybook parameters and docs page for demonstration

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -11,10 +11,19 @@ const meta = {
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  // tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
+    // backgroundColor: {
+    //   control: 'select',
+    //   options: ['red', 'blue', 'green', 'orange'],
+    // },
+  },
+  args: {
+    //👇 Now all Button stories will be primary.
+    primary: true,
+    label: 'Button',
   },
 } satisfies Meta<typeof Button>;
 
@@ -22,29 +31,20 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
+export const Primary: Story = {};
 
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
+export const Secondary: Story = {};
 
 export const Large: Story = {
   args: {
     size: 'large',
-    label: 'Button',
+    label: 'Large Button',
   },
 };
 
 export const Small: Story = {
   args: {
     size: 'small',
-    label: 'Button',
+    label: 'Small Button',
   },
 };
